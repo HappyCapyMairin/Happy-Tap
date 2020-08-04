b4ae387b96e8f072cc21dd7d8b886d27ea20619d
Mairin <mairincastellano@gmail.com>
2020-08-04 09:53:32 -0500

    Storyboard is set up with 3 view controllers. Set up "Tutorial" storyboard.

---
+++ Happy Tap/TutorialViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TutorialViewController.swift
+//  Happy Tap
+//
+//  Created by Mairin Jarvis Castellano on 8/3/20.
+//  Copyright © 2020 Mairin Jarvis Castellano. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+}
+}

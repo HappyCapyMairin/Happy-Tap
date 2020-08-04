8263a30ad2c9dfefcdec9cca47a49801f542e42b
Mairin <mairincastellano@gmail.com>
2020-08-04 15:07:03 -0500

    Set up game and got it to function for level 0.

---
+++ Happy Tap/TutorialVersionTwoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TutorialVersionTwoViewController.swift
+//  Happy Tap
+//
+//  Created by Mairin Jarvis Castellano on 8/4/20.
+//  Copyright © 2020 Mairin Jarvis Castellano. All rights reserved.
+//
+
+import UIKit
+
+class TutorialVersionTwoViewController: UIViewController {
+    @IBOutlet weak var countDownLabel: UILabel!
+    @IBOutlet weak var scoreDisplayLabel: UILabel!
+    var tapCount = 0
+    var countDown = 10
+    var timer = Timer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        runTimer()
+    }
+    
+    func runTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            print(self.countDown)
+            if self.countDown != 0 {
+                self.countDown -= 1
+                self.countDownLabel.text = String(self.countDown)
+            }
+            else {
+                print(self.tapCount)
+                self.timer.invalidate()
+                self.scoreDisplayLabel.text = "You got \(self.tapCount) in 10 seconds. Play again to beat your score!"
+                self.countDownLabel.text = ""
+            }
+        }
+    }
+    
+    @IBAction func whenButtonIsTapped(_ sender: Any) {
+        tapCount += 1
+    }
+    
+    
+}
--- Happy Tap/TutorialViewController.swift
+++ Happy Tap/TutorialViewController.swift
@@ -1,20 +1,59 @@
+////
+////  TutorialViewController.swift
+////  Happy Tap
+////
+////  Created by Mairin Jarvis Castellano on 8/3/20.
+////  Copyright © 2020 Mairin Jarvis Castellano. All rights reserved.
+////
 //
-//  TutorialViewController.swift
-//  Happy Tap
+//import UIKit
 //
-//  Created by Mairin Jarvis Castellano on 8/3/20.
-//  Copyright © 2020 Mairin Jarvis Castellano. All rights reserved.
+//class TutorialViewController: UIViewController {
+//    @IBOutlet weak var firstLabelTextField: UILabel!
+//    @IBOutlet weak var secondLabelTextField: UILabel!
+//    @IBOutlet weak var thirdLabelTextField: UILabel!
+//    @IBOutlet weak var fourthLabelTextField: UILabel!
+//    @IBOutlet weak var fifthLabelTextField: UILabel!
+//    @IBOutlet weak var labelDisplay: UILabel!
 //
-
-import UIKit
-
-class TutorialViewController: UIViewController {
-
- 
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-
-}
-}
+//
+//    override func viewDidLoad() {
+//        firstLabelTextField.isHidden = false
+//        secondLabelTextField.isHidden = true
+//        thirdLabelTextField.isHidden = true
+//        fourthLabelTextField.isHidden = true
+//        fifthLabelTextField.isHidden = true
+//        var counter = 0
+//        var textInLabel = labelDisplay.text!
+//
+//        super.viewDidLoad()
+//
+//    }
+//
+//    @IBAction func firstButtonIsPressed(_ sender: Any) {
+//        firstLabelTextField.isHidden = true
+//        secondLabelTextField.isHidden = false
+//        var counter = 1
+//    }
+//
+//    @IBAction func secondButtonIsPressed(_ sender: Any) {
+//        secondLabelTextField.isHidden = true
+//        thirdLabelTextField.isHidden = false
+//        var counter = 2
+//    }
+//
+//    @IBAction func thirdButtonIsPressed(_ sender: Any) {
+//        thirdLabelTextField.isHidden = true
+//        fourthLabelTextField.isHidden = false
+//        var counter = 3
+//    }
+//
+//    @IBAction func fourthButtonIsPressed(_ sender: Any) {
+//        fourthLabelTextField.isHidden = false
+//        fifthLabelTextField.isHidden = true
+//        var counter = 4
+//    }
+//    textInLabel = "\(counter)"
+//
+//
+//}

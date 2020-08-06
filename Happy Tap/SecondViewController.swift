5f222b816d0e65e2e8dbd0b5e8da680245052ebc
Mairin <mairincastellano@gmail.com>
2020-08-06 10:55:53 -0500

    added file to support gifs

--- Happy Tap/SecondViewController.swift
+++ Happy Tap/SecondViewController.swift
@@ -10,21 +10,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var gifView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gifView.loadGif(name: <#T##String#>)
 
-        // Do any additional setup after loading the view.
+      
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
 }
